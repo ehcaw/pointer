2193602ce9e204e931c01df07402478c0363ee4c
ehcaw <ryannguyenc@gmail.com>
2025-06-03 00:25:51 -0700

    started the mongodb backend

--- src-tauri/src/mongo.rs
+++ src-tauri/src/mongo.rs
@@ -12,4 +12,12 @@ impl MongoClient {
         let client = Client::with_uri_str(connection_string).await.unwrap();
         Self { client }
     }
+
+    async fn get_for_user(user_id: &str) {
+        let db = client.database("quibble");
+        let collection = db::collection::Document("notes");
+
+        let filter = doc! {tenantId: user_id};
+        let mut cursor = collection.find(filter, None).await?;
+    }
 }
