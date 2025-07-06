80d317b73bd35f1302f6e97388c77d8e402f8c3a
ehcaw <ryannguyenc@gmail.com>
2025-07-06 15:08:05 -0700

    removed groq rust file in favor of typescript one

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,10 +1,7 @@
-mod groq;
-use groq::create_completion;
-
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![create_completion])
+        .invoke_handler(tauri::generate_handler![])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
